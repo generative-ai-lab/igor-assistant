9e9617ac077f7b8e9e2faf11b70c29adfd503242
Nikita <nikitatarnik@gmail.com>
2024-04-11 15:21:30 +0300

    new UI, options for generation

--- bot/static_text.py
+++ bot/static_text.py
@@ -10,7 +10,11 @@ new_dialog_start = """Мы начали новый диалог"""
 
 home_page = """Выберите режим работы:"""
 
-image_mode = """Режим генерации изображений активирован. Пожалуйста,
-отправьте текстовое описание изображения.
+image_mode = """Режим генерации изображений активирован. 
+Пожалуйста, отправьте текстовое описание и выберите параметры генерации."""
 
-В данный момент по одному запросу генерируется одно изображение hd формата, 1024x1024, позже функционал будет расширен"""
+urls_mock = ["https://www.argospetinsurance.co.uk/assets/uploads/2017/12/cat-pet-animal-domestic-104827.jpeg",
+             "https://www.pets4homes.co.uk/images/articles/771/large/cat-lifespan-the-life-expectancy-of-cats-568e40723c336.jpg",
+             "https://www.cats.org.uk/uploads/images/featurebox_sidebar_kids/grief-and-loss.jpg",
+             "https://r.hswstatic.com/w_907/gif/tesla-cat.jpg"
+             ]
